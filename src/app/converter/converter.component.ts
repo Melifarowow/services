ca4b769e884a26b315bf6c128a3d0b7c2b5d8eca
Serhii Hromov <melifarowow@gmail.com>
2017-07-25 00:15:18 +0300

    converter temp

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConverterService } from './converter.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./converter.component.css']
 })
 export class ConverterComponent implements OnInit {
-
-  constructor() { }
+  converterForm: FormGroup
+  constructor(
+    private converterService: ConverterService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+  }
+
+  builtConverterForm() {
+    this.converterForm = this.formBuilder.group({
+      amount: ['10', [
+        Validators.required,
+        Validators.pattern('[0-9]+(\.[0-9]+)?'),
+        Validators.min(0.01),
+        Validators.max(1000000)
+      ]]
+    })
   }
 
 }
